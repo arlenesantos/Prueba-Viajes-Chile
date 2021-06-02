1a17203d9888086b3ac73c6b0b3761118d3441d3
Arlene Santos <arlenesantos89@gmail.com>
2021-06-01 21:07:48 -0400

    adiciona navegacion con smooth scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
+$(document).ready(function(){
 
+    // smooth scroll
+  $("a").click(function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var gato = this.hash;
+      $('html, body').animate({
+        scrollTop: $(gato).offset().top -40
+      }, 800);
+    }
+  });
+
+
+})
